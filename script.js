cea1f17be25762c549f654199b7dbc55730bd755
baladhaya75 <gammano34@gmail.com>
2025-07-18 19:27:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-<script>
-    // Show popup on page load
-    window.addEventListener("load", () => {
-        document.getElementById("welcomePopup").style.display = "flex";
-    });
-
-    // Close popup
-    function closePopup() {
-        document.getElementById("welcomePopup").style.display = "none";
-    }
-</script>
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
